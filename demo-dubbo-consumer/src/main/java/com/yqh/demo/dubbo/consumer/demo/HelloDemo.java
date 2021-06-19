b43de9e6e43da6fb3ed224ccb7951bcbdc24d75c
yangqh521 <yangqh521@qq.com>
2021-06-19 16:21:49 +0800

    dubbo demo 测试

--- demo-dubbo-consumer/src/main/java/com/yqh/demo/dubbo/consumer/demo/HelloDemo.java
+++ demo-dubbo-consumer/src/main/java/com/yqh/demo/dubbo/consumer/demo/HelloDemo.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author yangqh521
  * @version 1.0
@@ -25,6 +27,11 @@ public class HelloDemo {
     @Test
     public void sayHelloTest() {
         for (int i = 0; i < 10; i++) {
+            try {
+                TimeUnit.MILLISECONDS.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             helloService.sayHello("yangqh");
         }
     }
